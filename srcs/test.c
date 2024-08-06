8fbd5709a771ac7759293bdb186ccc190c618446
Eduardo Corona <eduardo.corona040@gmail.com>
2024-08-06 21:21:34 +0100

    Update test.c

--- srcs/test.c
+++ srcs/test.c
@@ -6,14 +6,52 @@
 /*   By: ecorona- <ecorona-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 18:31:34 by ecorona-          #+#    #+#             */
-/*   Updated: 2024/08/06 18:34:00 by ecorona-         ###   ########.fr       */
+/*   Updated: 2024/08/06 21:14:06 by ecorona-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 #include "libft.h"
 
-int main()
+typedef struct s_data
 {
-	return 1;
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		len;
+	int		endian;
+}	t_data;
+
+typedef unsigned char	t_uint8;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->len + x * (data->bpp / 8));
+	*(unsigned int *)dst = color;
+}
+
+int	argb(t_uint8 a, t_uint8 r, t_uint8 g, t_uint8 b)
+{
+	return (a << 24 | r << 16 | g << 8 | b);
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	(void)mlx_win;
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.len, &img.endian);
+	for (int x = 0; x < 50; x++)
+		for (int y = 0; y < 50; y++)
+			my_mlx_pixel_put(&img, 700+x, 700+y, argb(0x00, 0xb4, 0xd6, 0xcd));
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+	return (0);
 }
